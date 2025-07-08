e52db206f515044eeb91953b5aec420adb602165
Rika-aa <enricamuto@gmail.com>
2025-07-08 11:00:44 +0200

    Aggiunta modello oggetto libro e libreria

--- src/main/java/org/progetto/Model/GestoreLibreria.java
+++ src/main/java/org/progetto/Model/GestoreLibreria.java
@@ -1,4 +1,63 @@
 package org.progetto.Model;
 
-public class GestoreLibreria {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestoreLibreria implements Serializable {
+
+    List<Libro> libri;
+
+    public GestoreLibreria() {
+        libri = new ArrayList<Libro>();
+    }
+
+    public void aggiungiLibro(Libro lib) {
+        libri.add(lib);
+    }
+
+    public void modificaLibro(Libro lib) {
+        for(int i = 0; i < libri.size(); i++) {
+            Libro l = libri.get(i);
+            if(l.getISBN().equals(lib.getISBN())) {
+                libri.set(i, lib);
+                return;
+            }
+        }
+    }
+
+    public void eliminaLibro(Libro lib) {
+        libri.remove(lib);
+    }
+
+    public List<Libro> cercaLibroTitolo(String titolo) {
+        List<Libro> ret = new ArrayList();
+        for(Libro l : libri) {
+            if(l.getTitolo().equalsIgnoreCase(titolo)) {
+                ret.add(l);
+            }
+        }
+        return ret;
+    }
+
+    public List<Libro> cercaLibroAutore(String autore) {
+        List<Libro> ret = new ArrayList();
+        for(Libro l : libri) {
+            if(l.getAutore().equalsIgnoreCase(autore)) {
+                ret.add(l);
+            }
+        }
+        return ret;
+    }
+
+    public Libro cercaLibro(String ISBN){
+        for(Libro lib : libri){
+            if(lib.getISBN().equals(ISBN)){
+                return lib;
+            }
+        }
+        return null; //libro non trovato
+    }
+
 }
+
--- src/main/java/org/progetto/Model/Libro.java
+++ src/main/java/org/progetto/Model/Libro.java
@@ -1,4 +1,107 @@
 package org.progetto.Model;
 
-public class Libro {
+import java.io.Serializable;
+
+public class Libro implements Serializable{
+
+    public enum StatoLettura {
+        DA_LEGGERE,
+        IN_LETTURA,
+        LETTO
+    }
+
+    public enum Valutazione {
+        SCARSO(1,"Scarso"),
+        MEDIOCRE(2,"Mediocre"),
+        BUONO(3,"Buono"),
+        OTTIMO(4,"Ottimo"),
+        ECCELLENTE(5,"Eccellente");
+
+        private final int valore;
+        private final String descrizione;
+
+        Valutazione(int valore, String descrizione) {
+            this.valore = valore;
+            this.descrizione = descrizione;
+        }
+
+        public int getValore() {
+            return valore;
+        }
+
+        public String getDescrizione() {
+            return descrizione;
+        }
+
+    }
+
+    private String titolo, autore;
+    private String ISBN;
+    private String genere;
+    private Valutazione valutazione;
+    private StatoLettura statoLettura;
+
+    //Costruttore
+    public Libro(String titolo, String autore, String ISBN, String genere) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.ISBN = ISBN;
+        this.genere = genere;
+        this.statoLettura = StatoLettura.DA_LEGGERE; //caso di default
+    }
+
+    //Metodi accessori e mutatori
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    public Valutazione getValutazione() {
+        return valutazione;
+    }
+
+    public void setValutazione(Valutazione valutazione) {
+        this.valutazione = valutazione;
+    }
+
+    public StatoLettura getStatoLettura() {
+        return statoLettura;
+    }
+
+    public void setStatoLettura(StatoLettura statoLettura) {
+        this.statoLettura = statoLettura;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro: " + titolo + " di " + autore + "\n" +
+                "ISBN: " + ISBN + "\n" +
+                "Genere: " + genere + "\n" +
+                "Valutazione: " + (valutazione != null ? getValutazione().getDescrizione() : "Non valutato.") + "\n" +
+                "Stato lettura: " + statoLettura + "\n";
+    }
+
 }
